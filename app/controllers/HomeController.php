2d9cff74ac67a8ee9711d6fb5296bafb4fcb64d7
sathish <sathish.thi@gmail.com>
2014-05-11 22:28:22 +0530

    Fix for logout

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,17 +1,18 @@
 <?php
+
 use Services\Repository\IuserRepository;
 
-
 class HomeController extends BaseController {
-    private  $user;
-    public function __construct(IuserRepository $user){
+
+    private $user;
+
+    public function __construct(IuserRepository $user) {
         $this->user = $user;
-         
     }
+
     public function Index() {
 
         return View::make('hello');
-        
     }
 
     public function postLogin() {
@@ -21,25 +22,25 @@ class HomeController extends BaseController {
             'email' => Input::get('email'),
             'password' => Input::get('password')
         );
-        
+
 
         try {
             $user = $this->user->authenticate($credentials, false);
-            
+
             if ($user) {
-               
+
                 return Redirect::route('logins');
             }
         } catch (\Exception $e) {
-          
+
             return Redirect::route('login')->withErrors(array('login' => $e->getMessage()));
         }
     }
-    
-    public function logOut(){
-        var_dump($this->user->getLogout());
-   
-     //  var_dump($this->user->findAllUsers());
-       // return Redirect::route('login');
+
+    public function logOut() {
+        
+        $this->user->getLogout();
+        return Redirect::route('login');
     }
+
 }
